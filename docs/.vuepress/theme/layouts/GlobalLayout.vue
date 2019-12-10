5e184b964c2ad52aa0bf33f61c299ebea8656d42
Lucas Tostée <lucas.tostee@gmail.com>
2019-12-10 18:47:19 +0100

    Create GlobalLayout component use for all routes

---
+++ docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="moonshot-theme" class="container">
+    <Header/>
+    <!-- <component :is="layout"/> -->
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header";
+
+export default {
+  components: {
+    Header
+  },
+  computed: {
+    layout () {
+      if (this.$page.path) {
+        if (this.$frontmatter.layout) {
+          // You can also check whether layout exists first as the default global layout does.
+          return this.$frontmatter.layout
+        }
+        return 'Layout'
+      }
+      return 'NotFound'
+    }
+  },
+  mounted() {
+    document.querySelector("body").style = "padding:0;margin:0;";
+  }
+}
+</script>
