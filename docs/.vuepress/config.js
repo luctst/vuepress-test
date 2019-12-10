fbce94ae77a4a6101fbf650da0d6074d57bfa875
Lucas Tostée <lucas.tostee@gmail.com>
2019-12-10 18:44:33 +0100

    Create vuepress config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: "Moonshot-Internet - Homepage",
+    description: "Moonshot-Internet is an Insurtech which develops usage-based insurance products and services dedicated to E-Commerce. We have a flexible solution for your needs.",
+    markdown: {
+        lineNumbers: true
+    }
+}
