6e76e7181b949706cea7351c2c028904de46f63f
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-17 17:03:45 +0100

    Create newsletter component

---
+++ docs/.vuepress/theme/components/Newletter.vue
@@ -0,0 +1,135 @@
+<template>
+    <section class="container newsletter">
+        <div class="newsletter--background">
+            <div class="newsletter--content">
+                <h2>Souscrivez à la moonletter</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </div>
+            <input type="text" placeholder="Votre adresse mail"/>
+            <button>S'abonner</button>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+    .newsletter--background {
+        align-items: center;
+        background-image: url("../../public/group-6@2x.png");
+        background-position: center;
+        background-repeat: no-repeat;
+        display: flex;
+
+        .newsletter--content {
+        h2,
+        p {
+            color: #fff;
+        }
+
+        h2 {
+            margin: 0;
+            font-weight: 600;
+        }
+
+        p {
+            margin: 0;
+            line-height: 1.5;
+        }
+    }
+
+    input {
+        background-color: #ffffff;
+        border: none;
+        border-radius: 24px;
+        box-shadow: 0 4px 10px 0 rgba(40, 40, 40, 0.16);
+        color: #d3d5df;
+        padding: 8px 21px;
+    }
+
+    button {
+        border: none;
+        border-radius: 24px;
+        box-shadow: 0 4px 10px 0 rgba(40, 40, 40, 0.16);
+        background-image: linear-gradient(to bottom, #1cd9ec, #09c0d3);
+        color: #fff;
+        padding: 8px 16px;
+    }
+    }
+
+}
+
+@media screen and (min-width:300px) {
+    section {
+        margin-top: 10%;
+
+        .newsletter--background {
+            background-size: auto;
+            background-position: center;
+            border-radius: 24px;
+            flex-direction: column;
+            justify-content: center;
+            height: 40vh;
+
+            .newsletter--content {
+                text-align: center;
+
+                h2 {
+                    font-size: 22px;
+                }
+
+                p {
+                    font-size: 13px;
+                }
+            }
+
+            input {
+                margin: 15% 0 5% 0;
+            }
+        }
+
+    }
+}
+
+@media screen and (min-width: 400px) {
+    section {
+        .newsletter--background {
+            .newsletter--content {
+                h2 {
+                    font-size: 25px;
+                }
+
+                p {
+                    font-size: 15px;
+                }
+            }
+        }
+    }
+}
+
+@media screen and (min-width: 771px) {
+    section {
+        .newsletter--background {
+            height: 20vh;
+            flex-direction: row;
+            justify-content: left;
+            padding: 0 5%;
+
+            .newsletter--content {
+                text-align: left;
+            }
+
+            input {
+                margin: 0;
+                margin-left: auto;
+                margin-right: 2%; 
+            }
+        }
+    }
+}
+
+@media screen and (min-width: 900px) {
+    section {
+        margin-top: 4%;
+    }
+}
+</style>
