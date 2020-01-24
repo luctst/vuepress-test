bacc49276264a11d970ceeef23d8258795f2e319
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-24 18:14:08 +0100

    Add temoignages component

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -2,17 +2,20 @@
     <main>
         <AssuranceContextuelle/>
         <ProtectionClient />
+        <Temoignages />
     </main>
 </template>
 
 <script>
 import AssuranceContextuelle from "../components/AssuranceContextuelle";
 import ProtectionClient from "../components/ProtectionClient";
+import Temoignages from "../components/Temoignages";
 
 export default {
     components: {
         AssuranceContextuelle,
-        ProtectionClient
+        ProtectionClient,
+        Temoignages
     }
 }
 </script>
