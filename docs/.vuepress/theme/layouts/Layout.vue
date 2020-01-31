60c4023a24f3142b2ec241ccdb1b8e81376b4f82
Lucas Tostée <lucas.tostee@gmail.com>
2020-01-31 16:16:59 +0100

    Add accordion component, data

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,24 +1,64 @@
 <template>
-    <main>
-        <AssuranceContextuelle/>
-        <ProtectionClient />
-        <Temoignages />
-        <MshMedia />
-    </main>
+  <main>
+    <AssuranceContextuelle />
+    <Accordion :mapDataToProps="accordionData" />
+    <Temoignages />
+    <MshMedia />
+  </main>
 </template>
 
 <script>
 import AssuranceContextuelle from "../components/AssuranceContextuelle";
-import ProtectionClient from "../components/ProtectionClient";
+import Accordion from "../components/career/Accordion";
 import Temoignages from "../components/Temoignages";
-import MshMedia from "../components/MshMedia"
+import MshMedia from "../components/MshMedia";
 
 export default {
-    components: {
-        AssuranceContextuelle,
-        ProtectionClient,
-        Temoignages,
-        MshMedia
-    }
-}
+  components: {
+    AssuranceContextuelle,
+    Accordion,
+    Temoignages,
+    MshMedia
+  },
+  data() {
+    return {
+      accordionData: {
+        accordionTitle: "Nous vous protégeons vous et vos clients",
+        imgDesktop: "group-3065@2x.png",
+        accordionItem: [
+          {
+            title: "Voyage",
+            img: "group-3065@2x.png",
+            content:
+              "Retard de livraison de colis Livraison non conforme ou défectueuse Retour de produits"
+          },
+          {
+            title: "E-commerce",
+            img: "group-3065@2x.png",
+            content:
+              "Retard de livraison de colis Livraison non conforme ou défectueuse Retour de produits"
+          },
+          {
+            title: "Paiement",
+            img: "group-3065@2x.png",
+            content:
+              "Retard de livraison de colis Livraison non conforme ou défectueuse Retour de produits"
+          },
+          {
+            title: "Mobile",
+            img: "group-3065@2x.png",
+            content:
+              "Retard de livraison de colis Livraison non conforme ou défectueuse Retour de produits"
+          },
+          {
+            title: "Entertainment",
+            img: "group-3065@2x.png",
+            content:
+              "Retard de livraison de colis Livraison non conforme ou défectueuse Retour de produits"
+          }
+        ]
+      }
+    };
+  }
+};
 </script>
