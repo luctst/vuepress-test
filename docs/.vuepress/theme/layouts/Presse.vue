85bce41bd089079b86e46ba047654ca9952df5cb
Lucas Tostée <lucas.tostee@gmail.com>
2020-02-05 17:28:58 +0100

    Add bandeau, kitmedia component for the /press route

--- docs/.vuepress/theme/layouts/Presse.vue
+++ docs/.vuepress/theme/layouts/Presse.vue
@@ -1,9 +1,18 @@
 <template>
-    
+    <main class="container">
+        <Bandeau />
+        <KitMedia />
+    </main>
 </template>
 
 <script>
+import Bandeau from "../components/presse/Bandeau";
+import KitMedia from "../components/presse/KitMedia";
+
 export default {
-    
+    components: {
+        Bandeau,
+        KitMedia
+    }
 }
 </script>
