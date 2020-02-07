ae6648ee2e3a01a8358d1154c8675a3782a9ff41
Lucas Tostée <lucas.tostee@gmail.com>
2020-02-07 16:56:52 +0100

    Test adding env variables with dotenv package

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,17 @@
+require("dotenv").config();
+const webpack = require("webpack");
+
 module.exports = {
     title: "Moonshot-Internet - Homepage",
     description: "Moonshot-Internet is an Insurtech which develops usage-based insurance products and services dedicated to E-Commerce. We have a flexible solution for your needs.",
     markdown: {
         lineNumbers: true
+    },
+    configureWebpack () {
+        return {
+            plugins: [
+                new webpack.DefinePlugin({TOKEN: process.env.WTTJ_TOKEN})
+            ]
+        }
     }
 }
