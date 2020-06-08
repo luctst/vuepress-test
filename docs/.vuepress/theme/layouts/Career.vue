d3a1d32057f6272d7f23517bc669ab4f589e60d6
luctst <lucas.tostee@gmail.com>
2020-06-08 13:17:17 +0200

    Create spontane section and ending responsive

--- docs/.vuepress/theme/layouts/Career.vue
+++ docs/.vuepress/theme/layouts/Career.vue
@@ -1,8 +1,16 @@
 <template>
   <main>
     <section class="container offres">
-        <h2>Toutes nos offres d'emplois</h2>
-        <div id="welcomekit-embed"></div>
+      <h2>Toutes nos offres d'emplois</h2>
+      <div id="welcomekit-embed"></div>
+    </section>
+    <section class="spontane container">
+      <div class="spontane--title">
+        <h3>Vous n’avez pas trouvé la proposition de vos rêves ?</h3>
+        <p>Ce n’est pas grave, Moonshot-Internet est toujours à la recherche de nouveaux talents !</p>
+        <p>N’hésitez pas à nous contacter ou à nous envoyer votre candidature spontanée.</p>
+      </div>
+      <a>Candidature spontanée</a>
     </section>
     <Accordion :mapDataToProps="accordionData" />
   </main>
@@ -13,13 +21,12 @@ import Accordion from "../components/career/Accordion";
 
 export default {
   components: {
-      Accordion,
+    Accordion
   },
   data() {
     return {
       accordionData: {
-        accordionTitle:
-          "Une équipe de 45 experts du digital et de l’assurance.",
+        accordionTitle: "Découvrez Moonshot-Internet.",
         imgDesktop: "group-3065@2x.png",
         accordionItem: [
           {
@@ -61,55 +68,207 @@ export default {
 
 <style lang="scss">
 main {
-    .offres {
-        h2 {
-            color: #0e2244;
-            font-weight: bold;
-            text-align: center;
-        }
-
-        #welcomekit-embed {
-            ul {
-                list-style-type: none;
-                padding-left: 0;
-
-                .welcomekit-jobs-list-item {
-                    border: 1px solid #d3d5df;
-                    border-left-color: #1717ff;
-                    border-left-width: 7px;
-                    border-radius: 8px;
-                    padding: 0 24px;
-
-                    a {
-                        background-color: transparent;
-                        border-bottom-color: transparent;
-
-                        h3 {
-                            color: #0e2244;
-                        }
-
-                        ul {
-                            li {
-                                color: #d3d5df;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+  .offres {
+    h2 {
+      color: #0e2244;
+      font-family: "gotham-bold";
+      font-weight: bold;
+      margin: 0;
+      text-align: center;
     }
+
+    #welcomekit-embed {
+      ul {
+        list-style-type: none;
+        padding-left: 0;
+
+        .welcomekit-jobs-list-item {
+          border: 1px solid #d3d5df;
+          border-left-color: #1717ff;
+          border-left-width: 7px;
+          border-radius: 8px;
+          padding: 0 24px;
+
+          a {
+            background-color: transparent;
+            border-bottom-color: transparent;
+
+            h3 {
+              color: #0e2244;
+            }
+
+            ul {
+              li {
+                color: #d3d5df;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .spontane {
+    display: flex;
+
+    &--title {
+      h3 {
+        font-family: "gotham-medium";
+        font-weight: 500;
+      }
+
+      p {
+        font-family: "gotham-rounded";
+      }
+
+      h3,
+      p {
+        color: #38393f;
+        margin: 0;
+      }
+    }
+
+    a {
+      border-radius: 50px;
+      box-shadow: 0 4px 10px 0 rgba(135, 167, 171, 0.5);
+      background-image: linear-gradient(to bottom, #1cd9ec, #09c0d3);
+      color: #fff;
+      display: inline-block;
+      font-family: "gotham-medium";
+      width: max-content;
+    }
+  }
+}
+
+@media screen and (min-width: 300px) {
+  main {
+    .offres {
+      margin-top: 20%;
+
+      h2 {
+        font-size: 1.7em;
+        margin-bottom: 8%;
+      }
+
+      #welcomekit-embed {
+        ul {
+          .welcomekit-jobs-list-item {
+            margin-bottom: 8%;
+          }
+        }
+      }
+    }
+
+    .spontane {
+      flex-direction: column;
+      margin-top: 20%;
+      margin-bottom: 20%;
+
+      &--title {
+        h3 {
+          font-size: 1.7em;
+        }
+
+        p {
+          line-height: 22px;
+        }
+
+        p:first-of-type {
+          margin-top: 12%;
+        }
+
+        p:last-child {
+          margin-bottom: 12%;
+        }
+      }
+
+      a {
+        padding: 12px 22px;
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 460px) {
+  main {
+    .spontane {
+      margin-top: 16%;
+      margin-bottom: 16%;
+
+      &--title {
+        p:first-of-type {
+          margin-top: 8%;
+        }
+
+        p:last-child {
+          margin-bottom: 8%;
+        }
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 550px) {
+  main {
+    .spontane {
+      margin-top: 8%;
+      margin-bottom: 8%;
+
+      &--title {
+        p:first-of-type {
+          margin-top: 6%;
+        }
+
+        p:last-child {
+          margin-bottom: 6%;
+        }
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 768px) {
+  main {
+    .spontane {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+
+      &--title {
+        h3 {
+          margin-bottom: 8%;
+        }
+
+        p:first-of-type,
+        p:last-of-type {
+          margin: 0;
+        }
+      }
+
+      a {
+        height: max-content;
+      }
+    }
+  }
 }
 
 @media screen and (min-width: 1200px) {
     main {
-        .offres {
-            margin-top: 8%;
-
-            h2 {
-                font-size: 2em;
-                margin-bottom: 8%;
-            } 
+      .offres {
+        margin-top: 8%;
+        
+        h2 {
+          font-size: 2em;
         }
+
+        #welcomekit-embed {
+        ul {
+          .welcomekit-jobs-list-item {
+            margin-bottom: 3%;
+          }
+        }
+      }
     }
+  }
 }
 </style>
