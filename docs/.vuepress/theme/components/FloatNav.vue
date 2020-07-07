b57e03745a11f7053aa47b7a0d5006faa7e28097
luctst <lucas.tostee@gmail.com>
2020-07-07 17:40:06 +0200

    Start creatign floatNav component

---
+++ docs/.vuepress/theme/components/FloatNav.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="float--nav container">
+    <section class="desktop">
+      <div v-for="(l, i) in navData.items" :key="i">
+        <p>
+          <a>{{l}}</a>
+        </p>
+      </div>
+    </section>
+    <section class="mobile">
+      <select>
+        <option v-for="(e, i) in navData.items" :key="i">{{e}}</option>
+      </select>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    navData: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.float--nav {
+  .desktop {
+    display: flex;
+
+    div {
+      border: 1px solid #c3cee2;
+      border-radius: 20px;
+
+      p {
+        color: #c3cee2;
+        font-family: "gotham-rounded";
+        margin: 0;
+        text-align: center;
+      }
+
+      :hover {
+        cursor: pointer;
+      }
+    }
+  }
+
+  .mobile {
+      select {
+        -webkit-appearance: none;
+        border: none;
+        border-top: solid 1px #d3d5df;
+        border-bottom: solid 1px #d3d5df;
+        color: #09c0d3;
+        font-size: .8em;
+        font-weight: 500;
+        padding: 16px 0px 16px 5px;
+        width: 100%;
+      }
+  }
+}
+
+@media screen and (min-width: 300px) {
+    .float--nav {
+        margin-top: 16%;
+
+        .desktop {
+            display: none;
+        }
+
+        .mobile {
+            display: block;
+        }
+    }
+}
+</style>
